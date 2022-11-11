6daa9829cb7d195e2725747efe5ba6ec2b475128
brunodias-r <bruno.brd73@gmail.com>
2022-11-11 16:05:33 -0300

    Alteração para o consumo da API.

--- SectionsTimeline/src/components/timelineAcompanhamento/index.js
+++ SectionsTimeline/src/components/timelineAcompanhamento/index.js
@@ -1,153 +1,81 @@
-import { StyleSheet, Text, StatusBar, View, Dimensions } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import StepIndicator from 'react-native-step-indicator';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
-
-const labels = ["Cart", "Delivery Address", "Order Summary", "Payment Method", "Track"];
-
-const customStyles = {
-  stepIndicatorSize: 25,
-  currentStepIndicatorSize: 30,
-  separatorStrokeWidth: 2,
-  currentStepStrokeWidth: 3,
-  stepStrokeCurrentColor: '#fe7013',
-  stepStrokeWidth: 3,
-  stepStrokeFinishedColor: '#fe7013',
-  stepStrokeUnFinishedColor: '#aaaaaa',
-  separatorFinishedColor: '#fe7013',
-  separatorUnFinishedColor: '#aaaaaa',
-  stepIndicatorFinishedColor: '#fe7013',
-  stepIndicatorUnFinishedColor: '#ffffff',
-  stepIndicatorCurrentColor: '#ffffff',
-  stepIndicatorLabelFontSize: 13,
-  currentStepIndicatorLabelFontSize: 13,
-  stepIndicatorLabelCurrentColor: '#fe7013',
-  stepIndicatorLabelFinishedColor: '#ffffff',
-  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
-  labelColor: '#999999',
-  labelSize: 13,
-  currentStepLabelColor: '#fe7013'
-}
-
-
-export default function TimelineAcompanhamento() {
-  const [currentPosition, setCurrentPosition] = useState(0);
-  const data = [
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-    {
-      label: 'Ipsum Lorem',
-      status: 'In progress',
-      dateTime: 'Sun, 4th Nov 08:34PM'
-    },
-  ];
-  //http://10.136.36.40/fibonacci/index.json
-  console.disableYellowbox = true;
-  return (
-    <View style={styles.container}>
-      <StatusBar style={styles.statusBarStyle} barStyle="light-content">
-        <View style={styles.viewTwoStyle}>
-          <Text style={styles.textTwoStyle}>Acompanhamento</Text>
+export default class TimelineAcompanhamento extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.timeline}>
+          <View style={styles.parte1}>
+            <View style={styles.circulo} />
+            <View style={styles.linha}></View>
+          </View>
+          <View style={styles.parte2}>
+            <Text style={styles.titulo}>{this.props.data.nome}</Text>
+            <Text style={styles.descricao}>Carga Horária: {this.props.data.horas}</Text>
+            <Text style={styles.data}>Tipo: {this.props.data.cursoTipo.descricao}</Text>
+          </View>
         </View>
-        <View style={styles.viewThreeStyle}>
-          <StepIndicator
-            customStyles={customStyles}
-            currentPosition={currentPosition}
-            label={labels}
-            direction='vertical'
-            renderLabel={({position, stepStatus, label, currentPosition}) => {
-              return(
-                <View style={styles.lblContainer}>
-                  <Text>{data[position].label}</Text>
-                  <Text>{data[position].label}</Text>
-                  <Text>{data[position].label}</Text>
-                </View>
-              )
-            }}
-          >
-          </StepIndicator>
-        </View>
-      </StatusBar>
-    </View>
-  );
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1e1e1e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  statusBarStyle: {
-    backgroundColor: '#ee0aae',
-  },
-  viewTwoStyle: {
-    height: 55,
-    padding: 10,
-    width: '100%',
-    backgroundColor: '#000',
-    elevation: 10,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
     alignItems: 'center'
   },
-  textTwoStyle: {
-    color: '#f22f2f',
-    fontSize: 22,
-    fontWeitgh: 'bold'
+  timeline: {
+    flexDirection: 'row',
+    width: '100%',
   },
-  viewThreeStyle: {
-    height: height,
-    width: width,
-    padding: 20,
-    paddingTop: 1,
-    margin: 15,
-    elevation: 10,
-    borderRadious: 20,
-    backgroundColor: '#000',
+  tituloTimeline: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    justifyContent: 'center'
+  },
+  parte1: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingRight: 5,
+  },
+  circulo: {
+    width: 30,
+    height: 30,
+    borderRadius: '50%',
+    backgroundColor: '#00315a',
+  },
+  linha: {
+    backgroundColor: '#00315a',
+    height: '100%',
+    width: 2,
+    justifyContent: 'center',
+    
+  },
+  parte2: {
+    flex: 7,
+    padding: 5,
+    paddingTop: 0,
+    backgroundColor: '#C5C5C5',
+    borderRadius: 10,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  descricao: {
+    fontSize: 15,
+    color: '#000'
+  },
+  data: {
+    fontSize: 15,
+    color: '#656565',
   },
 });
